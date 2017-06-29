15e6b9cddf24a637f2dd3891de3f2c9b4291e494
Nimrod Bahar <nimrodbahar@gmail.com>
2017-06-29 17:54:18 +0200

    added show component and wired it to the relevant piece of state object

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState = { ...state };
+      // newState[post.id] = post;           assign to the key: id prop on the post object, the post obeject
+      // return newState;
+
+      return { ...state, [action.payload.data.id]: action.payload.data} //es6 key interpolation
     case FETCH_POSTS:
       return _.mapKeys((action.payload.data), 'id');
     default:
